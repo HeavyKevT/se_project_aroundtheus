0e88f922298c8f82ac56b2dc60bc7a8899020d21
Kevin Taylor <K.A.Taylor@protonmail.com>
2024-05-01 10:17:21 -0600

    stage_1_array

---
+++ scripts/index.js
@@ -0,0 +1,32 @@
+initialCards = [
+    const card = {
+        name = "Glencoe, Scotland",
+        link = "https://unsplash.com/photos/green-and-brown-mountains-beside-river-under-blue-sky-and-white-clouds-during-daytime-mKjXta9DGc8"
+    }
+
+    const card = {
+        name = "Nāpali Coast, Hawaii",
+        link = "https://unsplash.com/photos/brown-and-green-mountain-ridge-near-body-of-water-Jat5D3lH_FA"
+    }
+
+    const card = {
+        name = "Banff National Park, Canada",
+        link = "https://unsplash.com/photos/lake-by-the-mountains-near-pine-trees-lWMMq8Hh6Dg"
+    }
+    
+    const card = {
+        name = "Uyuni Salt Flat, Bolivia",
+        link = "https://unsplash.com/photos/white-clouds-under-blue-sky-7BG3z3N6zAI"
+    }
+
+    const card = {
+        name = "Le Morne Brabant, Mauritius",
+        link = "https://unsplash.com/photos/aerial-photography-of-sea-near-mountain-at-daytime-WV4B_aVj0aQ"
+    } 
+
+    const card = {
+        name = "Torres Del Paine, Chile",
+        link = "https://unsplash.com/photos/snow-covered-mountain-in-reflective-photography-vmra8DWORZc"
+    } 
+
+];
