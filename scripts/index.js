ce340687c7aee8c3d40e734b111c18ba1e7595b4
Kevin Taylor <K.A.Taylor@protonmail.com>
2024-05-01 10:33:43 -0600

    fix_index.js_syntax

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,31 @@
 initialCards = [
-    const card = {
-        name = "Glencoe, Scotland",
-        link = "https://unsplash.com/photos/green-and-brown-mountains-beside-river-under-blue-sky-and-white-clouds-during-daytime-mKjXta9DGc8"
-    }
+  {
+    name: "Glencoe, Scotland",
+    link: "https://unsplash.com/photos/green-and-brown-mountains-beside-river-under-blue-sky-and-white-clouds-during-daytime-mKjXta9DGc8",
+  },
 
-    const card = {
-        name = "Nāpali Coast, Hawaii",
-        link = "https://unsplash.com/photos/brown-and-green-mountain-ridge-near-body-of-water-Jat5D3lH_FA"
-    }
+  {
+    name: "Nāpali Coast, Hawaii",
+    link: "https://unsplash.com/photos/brown-and-green-mountain-ridge-near-body-of-water-Jat5D3lH_FA",
+  },
 
-    const card = {
-        name = "Banff National Park, Canada",
-        link = "https://unsplash.com/photos/lake-by-the-mountains-near-pine-trees-lWMMq8Hh6Dg"
-    }
-    
-    const card = {
-        name = "Uyuni Salt Flat, Bolivia",
-        link = "https://unsplash.com/photos/white-clouds-under-blue-sky-7BG3z3N6zAI"
-    }
+  {
+    name: "Banff National Park, Canada",
+    link: "https://unsplash.com/photos/lake-by-the-mountains-near-pine-trees-lWMMq8Hh6Dg",
+  },
 
-    const card = {
-        name = "Le Morne Brabant, Mauritius",
-        link = "https://unsplash.com/photos/aerial-photography-of-sea-near-mountain-at-daytime-WV4B_aVj0aQ"
-    } 
+  {
+    name: "Uyuni Salt Flat, Bolivia",
+    link: "https://unsplash.com/photos/white-clouds-under-blue-sky-7BG3z3N6zAI",
+  },
 
-    const card = {
-        name = "Torres Del Paine, Chile",
-        link = "https://unsplash.com/photos/snow-covered-mountain-in-reflective-photography-vmra8DWORZc"
-    } 
+  {
+    name: "Le Morne Brabant, Mauritius",
+    link: "https://unsplash.com/photos/aerial-photography-of-sea-near-mountain-at-daytime-WV4B_aVj0aQ",
+  },
 
+  {
+    name: "Torres Del Paine, Chile",
+    link: "https://unsplash.com/photos/snow-covered-mountain-in-reflective-photography-vmra8DWORZc",
+  },
 ];
